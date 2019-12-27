cf842a533d70d0f2423a80369765a47748e203b4
Johnny Ly <johnbus.ly@gmail.com>
2019-12-26 20:27:20 -0800

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,10 @@
+public class FirstProgram {
+
+	public static void main(String[] args) {
+		//The main is the main part of the program
+		System.out.println("Hello World!!!");
+		System.out.println("My name is Johnny Ly.");
+		System.out.print("Welcome to CSE20 at UC Merced.");
+	}
+
+}
