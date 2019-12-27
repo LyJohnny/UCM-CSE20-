5b5e1ebc3f3c0e3b3c829dfb3379d36d3abc35ca
Johnny Ly <johnbus.ly@gmail.com>
2019-12-26 22:02:46 -0800

    Import Test and Interview Questions
    Testing Import Statement and more Print Statements utilizing the Tab Character "\t"

---
+++ Interview.java
@@ -0,0 +1,15 @@
+
+public class Interview {
+
+	public static void main(String[] args) {
+		System.out.print("Person interviewed:");
+		System.out.println(" \tEthan");
+		System.out.println("Ethan's hometown: \tSan Jose");
+		System.out.println("Ethan's major: \t\tComputer Science and Engineering");
+		System.out.print("Ethan's class standing:");
+		System.out.println("\tFirst Year");
+		System.out.print("This is the second CSE20 Lab.");
+		
+	}
+
+}
