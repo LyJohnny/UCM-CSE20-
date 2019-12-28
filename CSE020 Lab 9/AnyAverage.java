e17a4dfd0d01668dae2dde6aab1957d54e62471a
Johnny Ly <johnbus.ly@gmail.com>
2019-12-27 22:37:05 -0800

    Calculates Average of any amount

--- CSE020 Lab 9/AnyAverage.java
+++ CSE020 Lab 9/AnyAverage.java
@@ -6,6 +6,9 @@ public class AnyAverage {
 		System.out.println("This program will find the average of as many numbers as you like.");
 		Scanner inputA = new Scanner(System.in);
 		int numAmt;
+		
+		//Asks how many values the user wants to average
+		
 		System.out.println("\nPlease choose the number of values to average: ");
 		numAmt = inputA.nextInt();
 		int currentCount = 0;
@@ -13,6 +16,10 @@ public class AnyAverage {
 		int numToAdd = 0;
 		int total = 0;
 		Scanner inputB = new Scanner(System.in);
+		
+		/*This asks the user for each input and increments the currentCount till 
+		it reaches the maximum numAmt the user initially put. */
+		
 		while(currentCount < numAmt) {
 			System.out.println("Please enter the " + num + " number: ");
 			num++;
@@ -21,9 +28,11 @@ public class AnyAverage {
 			currentCount++;
 			
 		}
+		//Calculates average
 		if (numAmt > 0) {
 			System.out.println("The average of all the numbers is: " + (double)(total/numAmt));
 		}
+		//If user inputs zero, the program will end
 			else if (numAmt == 0) {
 				System.out.println("The average of all the numbers is 0.0" );
 			}
