7b83f91cfdd2e0f313ef26cc59a81f5485fd978b
Johnny Ly <johnbus.ly@gmail.com>
2019-12-27 23:18:18 -0800

    Calculating sum of numbers squared

--- CSE020 Lab 10/SumSquare.java
+++ CSE020 Lab 10/SumSquare.java
@@ -7,11 +7,12 @@ public class SumSquare {
 		System.out.println("Please enter the maximum number: ");
 		max = inputN.nextInt();
 		int total = 0;
-		
+		//Outputs the square of a number and increments it till the maximum count which is the user input
 		for(int i = 1; i <= max; i++) {
 			System.out.println("Number " + i + " squared is " + (i*i));
 			 total += i*i;
 		}
+		//Calculates the total of numbers squared altogether
 		System.out.println("\nThe sum of squares of all numbers from " + (max- (max-1)) + " till " + max + " is: " + total);
 	}
 
